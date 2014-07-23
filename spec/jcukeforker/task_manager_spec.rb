daff250a7e620a6dfec5a0fdb64e9ac29c32350b
jesg <savagejas@gmail.com>
2014-07-23 05:21:52 -0500

    add spec for task manager

---
+++ spec/jcukeforker/task_manager_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path("../../spec_helper", __FILE__)
+
+module JCukeForker
+  describe TaskManager do
+    let(:worker_path) { '/tmp/jcukeforker-test-socket' }
+    let(:task) { {name: 'my task'} }
+    let(:mock_socket) { double('socket', :puts => nil) }
+
+    it "can register a worker" do
+
+      UNIXSocket.stub(:open).and_return(mock_socket)
+      expect(mock_socket).to receive(:puts).with(task.to_json)
+
+      task_manager = TaskManager.new
+      task_manager.add task
+      task_manager.on_worker_register worker_path
+    end
+
+    it "can finish task" do
+
+      expect(mock_socket).to receive(:puts).with(task.to_json)
+
+      task_manager = TaskManager.new
+      task_manager.add task
+      task_manager.instance_variable_get(:@worker_sockets)[worker_path] = mock_socket
+      task_manager.on_task_finished worker_path, nil, nil
+    end
+
+    it "can close dead worker" do
+
+      expect(mock_socket).to receive(:close)
+
+      task_manager = TaskManager.new
+      task_manager.add task
+      task_manager.instance_variable_get(:@worker_sockets)[worker_path] = mock_socket
+      task_manager.on_worker_dead worker_path
+    end
+  end
+end
